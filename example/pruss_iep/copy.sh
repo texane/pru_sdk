3bfe57cab64d4f57dd10d2e359d267cacafa43d1
texane <texane@gmail.com>
2015-05-22 22:52:09 +0200

    [ add ] pruss_iep scripts

---
+++ example/pruss_iep/copy.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env sh
+
+scp \
+pru_enable-00A0.dtbo \
+iep.bin \
+main \
+run.sh \
+root@192.168.0.44:/tmp/
--- example/pruss_iep/run.sh
+++ example/pruss_iep/run.sh
@@ -1,3 +1,11 @@
 #!/usr/bin/env sh
-echo pru_enable > /sys/devices/bone_capemgr.9/slots
-./main
+
+umount /lib/firmware
+mount /dev/mmcblk0p3 /lib/firmware
+cp /tmp/gpio_p8_11-00A0.dtbo /lib/firmware/
+
+echo gpio_p8_11 > /sys/devices/bone_capemgr.9/slots
+
+modprobe uio_pruss
+
+/tmp/main
